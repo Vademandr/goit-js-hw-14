b8abc9d6db60146127de225c9b16505460c9dae0
Vademandr <140320640+Vademandr@users.noreply.github.com>
2023-10-17 02:46:44 +0300

    fix5

--- js/task-01.js
+++ js/task-01.js
@@ -4,9 +4,9 @@ const items = categoriesList.querySelectorAll('.item');
 console.log(`Total categories: ${items.length}`);
 
 items.forEach(item => {
-  const title = item.querySelector('h2').textContent;
-  const subCategories = item.querySelectorAll('li');
-    
-  console.log(`Category: ${title}`);
+  const title = item.firstElementChild;
+  const subCategories = item.lastElementChild.children;
+
+  console.log(`Category: ${title.textContent}`);
   console.log(`Number of items: ${subCategories.length}`);
 });
